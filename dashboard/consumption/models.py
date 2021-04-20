eb1cb0093929ba66179bd5b11d210ed6e9261eaa
Makintosh0121 <makoto.shirasu.2015@globis.ac.jp>
2021-04-20 19:26:24 +0900

    feat: data_modelを実装.

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(unique=True)
+    area = models.TextField(max_length=100)
+    tariff = models.TextField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user_id}, {self.area}, {self.tariff}"
+
+class Consumption(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    consumption = models.FloatField()
+
+    def __str__(self):
+        return f"{self.user}, {self.datetime}, {self.consumption}"
