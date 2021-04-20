8cb68fc26c0bb8c76d093a6a31d483e7446faa6d
Makintosh0121 <makoto.shirasu.2015@globis.ac.jp>
2021-04-20 19:28:21 +0900

    feat: import.pyを実装.

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,30 @@
 from django.core.management.base import BaseCommand
+from consumption.utils import DataLoader, UserValidator, ConsumptionValidator
 
+import os
+import glob
+import configparser
+
+config_ini = configparser.ConfigParser()
+config_ini.read("config.ini", encoding="utf-8")
+user_data_path = config_ini.get("PATH_SETTINGS", "USER_DATA")
+consumption_data_path = config_ini.get("PATH_SETTINGS", "CONSUMPTION_DATA")
+file_paths = (user_data_path, consumption_data_path)
 
 class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        is_valid = True
+        user_validator = UserValidator(user_data_path)
+        consumption_files = glob.glob(consumption_data_path + "/*.csv")
+        for consumption_file in consumption_files:
+            consumption_validator = ConsumptionValidator(consumption_file)
+            if not consumption_validator.is_valid():
+                is_valid = False
+        
+        if is_valid:
+            print(f"All data is valid.")
+            dataloader = DataLoader(file_paths=file_paths)
+            dataloader.register_user()
+            dataloader.register_consumption()
